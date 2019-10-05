aef6ebfe15a967e6d40f8e25d795206e1b63b3c3
Jashele T <jashele@Jasheles-Air.domain>
2019-10-04 18:24:42 -0700

    forgot to push dashboard test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+
+import React from "react";
+import Dashboard from "./Dashboard.js";
+import { render } from "@testing-library/react";
+
+//renders the component
+test("Component renders", () => {
+  render(<Dashboard />);
+});
+
+test("shows controls and display", () => {
+    const { getByText } = render(<Dashboard />);
+
+    const display = getByText(/open/i)
+    const controls = getByText(/close gate/i)
+
+    expect(display).toBeDefined();
+    expect(controls).toBeDefined();
+})
